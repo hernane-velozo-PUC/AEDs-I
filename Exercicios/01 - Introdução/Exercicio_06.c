e27d842d0f677da8e993655c1a9dd003924cbec9
Hernane PUC <hernane.rosa@sga.pucminas.br>
2021-12-31 17:38:12 -0300

    Exercicios

---
+++ Exercicios/01 - Introdução/Exercicio_06.c
@@ -0,0 +1,29 @@
+/***************************************************************************************************************
+
+Pontifícia Universidade Católica de Minas Gerais | Algoritmos e Estruturas de Dados I
+
+# Autor: Hernane Velozo                                                                                          
+#Exercício: 
+
+6) Faça um algoritmo que solicita para o usuário um número inteiro n (digitado) e posteriormente 
+o  sistema mostra o sucessor de n (ex. se o usuário digitou 15, o sistema deverá imprimir 16).
+
+***************************************************************************************************************/
+
+#include <stdio.h>
+
+int main(){
+
+    //Definicao de variavel
+    int sucessor;
+
+
+    //Impressão e leitura do usuário
+    printf("\nInforme um numero: ");
+    scanf("%d", &sucessor);
+
+    //apresentação na tela do usuário
+    printf("\nO valor digitado foi: %d e o seu sucessor eh: %d\n", sucessor, sucessor+1);
+
+    return 0;
+}
---
+++ Exercicios/01 - Introdução/Exercicio_07.c
@@ -0,0 +1,40 @@
+/***************************************************************************************************************
+
+Pontifícia Universidade Católica de Minas Gerais | Algoritmos e Estruturas de Dados I
+
+# Autor: Hernane Velozo                                                                                          
+#Exercício: 
+
+7) Faça um algoritmo que solicita o valor de 3 notas (n1, n2 e n3) e depois mostra: a soma e a média das notas.
+***************************************************************************************************************/
+
+#include <stdio.h>
+
+int main(){
+
+    //Definicao de variavel
+    float n1, n2, n3, soma, media;
+
+    //Impressão e leitura do usuário
+    printf("\nInforme a primeira nota: ");
+    scanf("%f", &n1);
+
+    printf("\nInforme a segunda nota: ");
+    scanf("%f", &n2);
+
+    printf("\nInforme a terceira nota: ");
+    scanf("%f", &n3);
+
+    //calculo da operação
+    soma = n1 + n2 + n3;    
+    media = soma / 3;
+
+    //apresentação na tela do usuário
+    printf("\nNotas informadas: %.2f, %.2f e %.2f", n1, n2, n3);
+    printf("\nSoma....: %.2f", soma);
+    printf("\nMedia...: %.2f\n", media);
+
+    printf("\n\n");
+
+    return 0;
+}
